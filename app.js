06e2701864518edadc9525edbeae9f7fc4db7b85
Kevin Kenfack <kenfackjoelkevin@yahoo.com>
2024-09-24 12:21:48 +0100

    deploy to Railway APP

---
+++ app.js
@@ -0,0 +1,24 @@
+'use strict';
+const config = require('config');
+const logger = require('./lib/logger')();
+const database = require('./lib/database');
+const Bot = require('./lib/bot');
+const priceTracker = require('./lib/price-tracker');
+const Alert = require('./lib/templates/alert');
+
+const mongoConnectionURI = config.get('mongo.connectionURI');
+const telegramBotToken = config.get('telegram.token');
+
+const bot = new Bot(telegramBotToken);
+
+database.connect(mongoConnectionURI).then(() => {
+  bot.launch();
+  priceTracker.start();
+
+  priceTracker.on('update', product => {
+    bot.sendMessage(product.user, new Alert(product).toMarkdown());
+    logger.info(`Alerte envoyée : ${product.name} (${product.id})`);
+  });
+
+  logger.info('Pricegram a démarré...');
+});
